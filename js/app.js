a1c033ddd1d93460fec56496f1cdda5d53e26aa2
thara-messeroux <tharamesseroux@gmail.com>
2025-12-18 21:31:27 -0500

    Document class vs object mental model

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ console.log("Hello, World!");
 A class is a blueprint (plan) for creating objects.
 Objects are created from the class later.
 */
+
+/* 
+CLASS vs OBJECT
+
+Class = blueprint (not real)
+Object = thing created from the class (real)
+
+One class → many objects
+*/
+
